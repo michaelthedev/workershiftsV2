b589eb625da7756313108fd69e22695071f190c8
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-22 07:10:51 +0100

    chore: Added configurations

---
+++ config/app.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'redis' => [
+        'enabled' => $_SERVER['REDIS_ENABLED'],
+        'url' => $_SERVER['REDIS_URL'],
+    ],
+];
---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => $_SERVER['DB_HOST'],
+    'name' => $_SERVER['DB_NAME'],
+    'port' => $_SERVER['DB_PORT'],
+    'username' => $_SERVER['DB_USER'],
+    'password' => $_SERVER['DB_PASS'],
+];
