ce0e57c1c19b9138a38d2b7535350f319d8c2002
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-22 07:13:31 +0100

    Removed laravel collection, using entities

--- app/Repositories/WorkerRepository.php
+++ app/Repositories/WorkerRepository.php
@@ -3,10 +3,7 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
-use App\Models\Worker;
-use App\Repositories\DTO\WorkerDTO;
-use App\Repositories\Interfaces\WorkerRepositoryInterface;
-use Illuminate\Database\Eloquent\Collection;
+use App\Entities\Worker;
 
 class WorkerRepository implements WorkerRepositoryInterface
 {
