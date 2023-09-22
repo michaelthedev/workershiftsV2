a7a33666c4237ef27ad9b1ef772ca7a1abc14374
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-22 23:08:13 +0100

    Create BaseController.php

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class BaseController
+{
+    final protected function sanitizeInput(string $value): string
+    {
+        return htmlspecialchars(trim($value));
+    }
+
+    final protected function successResponse(array $data, int $statusCode = 200, string $message = 'success'): void
+    {
+        response()->httpCode($statusCode)->json([
+            'error' => false,
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+
+    final protected function errorResponse(array $data, int $statusCode = 500, string $message = 'error'): void
+    {
+        response()->httpCode($statusCode)->json([
+            'error' => false,
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+}
