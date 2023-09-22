2f474e2b33460da49fd6673de74d22c27f24e8b5
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-22 07:09:37 +0100

    Added Dotenv

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,9 +2,20 @@
 
 // Constants
 define("BASE_PATH", dirname(__FILE__, 2));
+
 const ROUTES_PATH = BASE_PATH . '/routes';
 const CONFIG_PATH = BASE_PATH . '/config';
 
 // Include the composer autoloader
 require BASE_PATH.'/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$envFile = BASE_PATH . '/.env';
+if (file_exists($envFile)) {
+    $dotenv->load();
+}
+$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_PORT']);
+
+if (config('app.redis.enabled')) {
+    $dotenv->required(['REDIS_URL']);
+}
