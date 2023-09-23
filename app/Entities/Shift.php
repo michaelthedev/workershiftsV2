8b353804cfc1de93a8cc8abf22bdc5533c401b3b
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-23 17:23:47 +0100

    chore: getters and setters

--- app/Entities/Shift.php
+++ app/Entities/Shift.php
@@ -18,9 +18,19 @@ final class Shift
         return $this->id;
     }
 
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
     public function getWorkerId(): int
     {
         return $this->workerId;
+    }
+
+    public function setWorkerId(int $workerId): void
+    {
+        $this->workerId = $workerId;
     }
 
     public function getStart(): DateTimeImmutable
@@ -28,9 +38,28 @@ final class Shift
         return $this->start;
     }
 
+    public function setStart(DateTimeImmutable $start): void
+    {
+        $this->start = $start;
+    }
+
     public function getEnd(): DateTimeImmutable
     {
         return $this->end;
     }
 
+    public function setEnd(DateTimeImmutable $end): void
+    {
+        $this->end = $end;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'worker_id' => $this->workerId,
+            'start' => $this->start->format('Y-m-d\TH:i'),
+            'end' => $this->end->format('Y-m-d\TH:i')
+        ];
+    }
 }
